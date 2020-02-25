a0384dc2b5d530876779a26453204063ddbc2339
Vlad <NightlyDreamer@users.noreply.github.com>
2020-02-25 17:01:23 +0200

    add folder components changed the structure of the project

---
+++ src/components/search-panel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    const SearchText = 'Hi';
+    return <input placeholder={SearchText} />;
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = () => {
-    return <h1>Todo List</h1>;
-};
-
-const SearchPanel = () => {
-    return <input placeholder="Search someone..." />;
-};
-
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Props</li>
-            <li>State</li>
-            <li>Fetch API</li>
-            <li>Life Component</li>
-            <li>React Hooks</li>
-            <li>React Router</li>
-        </ul>
-    );
-;}
+import Header from './components/header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
+    
     return (
         <div>
             <Header />
